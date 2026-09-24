46a99db58b39dba098813aace7a2cbae30338dd4
kadir teker <kate1000@stud.ek.dk>
2026-09-24 22:12:40 +0200

    Add files via upload

---
+++ sp1.java
@@ -0,0 +1,29 @@
+package semester1;
+
+public class sp1 {
+
+
+
+    void main () {
+
+        String name = "Ragnar";
+        String type = "Warrior";
+        int healthPoints = 85;
+        int maxHealth = 100;
+        int level = 5;
+        int experiencePoints = 2300;
+        double gold = 156.50;
+        boolean isAlive = true;
+
+        String [] items = {"sword", "shield", "potion", "bow"};
+
+        System.out.println("Name:" + name + "\n" + "Class:" + type + "\n" + "Health:" + healthPoints + "\n" + "Max health:" +  maxHealth + "\n" + "level:" + level + "\n" +  "XP:" + experiencePoints + "\n" + "gold:" + gold + "\n" +"Alive:" + isAlive);
+
+
+
+
+
+
+
+    }
+}
